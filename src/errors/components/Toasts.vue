f5a5bbadf23e9d937f016aa7a63096949a91084e
melon <cccaaarrrlll99@gmail.com>
2025-04-07 17:43:34 +0800

    update html and css of Error Toasts component

--- src/errors/components/Toasts.vue
+++ src/errors/components/Toasts.vue
@@ -4,6 +4,7 @@ import IconClose from "~icons/material-symbols/close";
 import { ref, computed, watch } from "vue";
 import { type Error, errors, deleteError } from "../store";
 
+const tempDisplayDuration = 5000;
 const errors_TempDisplay = ref<typeof errors.value>([]);
 const displayAllErrors = ref(false);
 
@@ -28,7 +29,7 @@ watch(
 			if (newErrorIndex === -1) return;
 
 			errors_TempDisplay.value.splice(newErrorIndex, 1);
-		}, 5000);
+		}, tempDisplayDuration);
 	},
 );
 
@@ -46,7 +47,10 @@ function toggleErrorFocus(id: Error["id"]) {
 </script>
 
 <template>
-	<div v-if="errors.length" class="fixed right-10 top-10 z-20">
+	<div
+		v-if="errors.length"
+		class="ignore-default-boundaries fixed right-10 top-32 z-20 drop-shadow-xl"
+	>
 		<button
 			type="button"
 			:class="[
@@ -69,13 +73,14 @@ function toggleErrorFocus(id: Error["id"]) {
 
 		<ul
 			:class="[
-				{ 'bg-stone-50/25': displayAllErrors },
-				'mt-3 flex max-h-[31.25rem] flex-col gap-1 overflow-auto p-2',
+				{ 'bg-neutral-700/25': displayAllErrors },
+				'ml-auto mt-3 flex max-h-[31.25rem] w-fit flex-col items-end gap-1 overflow-auto p-2',
 			]"
 		>
 			<li
 				v-for="error in errors_ListDisplay"
 				:id="error.id"
+				:key="error.id"
 				class="flex"
 				data-focused="false"
 				@click="toggleErrorFocus(error.id)"
@@ -87,15 +92,20 @@ function toggleErrorFocus(id: Error["id"]) {
 					<IconClose />
 				</button>
 
-				<div class="w-[40ch] cursor-pointer bg-red-400 px-4 py-2 text-white">
+				<div
+					id="content"
+					class="cursor-pointer bg-red-400 px-4 py-2 text-white"
+				>
 					<div class="flex items-start justify-between gap-2">
 						<span id="title" class="text-xl font-bold">{{ error.title }}</span>
 						<span id="date-added" class="mt-[0.4rem] whitespace-nowrap text-xs">
-							● {{ new Date(error.dateAdded).toLocaleTimeString() }}
+							● &nbsp {{ new Date(error.dateAdded).toLocaleTimeString() }}
 						</span>
 					</div>
 
-					<p id="desc" class="text-sm">{{ error.desc }}</p>
+					<pre id="desc" class="text-wrap font-[inherit]/ text-sm">{{
+						error.desc
+					}}</pre>
 				</div>
 			</li>
 		</ul>
@@ -113,6 +123,10 @@ function toggleErrorFocus(id: Error["id"]) {
 
 ul {
 	scrollbar-color: theme(colors.red.400) #0000;
+}
+
+li[data-focused="false"] #content {
+	@apply w-[40ch];
 }
 
 li[data-focused="false"] #title {
