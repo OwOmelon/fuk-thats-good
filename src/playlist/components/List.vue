f49de1ca85462c9882a6edb1e2e0fab55564ef27
melon <cccaaarrrlll99@gmail.com>
2025-04-07 18:00:17 +0800

    refactor + add tiny fixes/changes to play list component

--- src/playlist/components/List.vue
+++ src/playlist/components/List.vue
@@ -3,12 +3,11 @@ import PlaylistItem_Component from "./Item.vue";
 
 import { useTemplateRef, computed, onMounted } from "vue";
 import { storeToRefs } from "pinia";
-import { getTime } from "date-fns";
 import { useDataStore } from "../stores/data";
 import { useFiltersStore } from "../stores/filters";
 import { useVideoTagsStore } from "../stores/video_tags";
 
-import type { PlaylistItem } from "../types";
+import type { PlaylistItems_ListResponse } from "../types";
 
 const bottom = useTemplateRef<HTMLElement>("bottom");
 
@@ -26,43 +25,16 @@ const currentPlaylist = computed(() => {
 	return playlists.value[activeYear.value];
 });
 
-const playlistItems_Sorted = computed(() => {
-	if (!currentPlaylist.value?.data) return [];
-
-	return currentPlaylist.value.data.items.toSorted(
-		sortDescending.value ? desc : asc,
+const currentPlaylistItems = computed(() => {
+	let items = JSON.parse(
+		JSON.stringify(currentPlaylist.value.data?.items ?? []),
 	);
 
-	function getPublishTime(a: PlaylistItem, b: PlaylistItem) {
-		return {
-			timeA: getTime(new Date(a.snippet.publishedAt)),
-			timeB: getTime(new Date(b.snippet.publishedAt)),
-		};
-	}
+	items = filterPlaystListItemsBySearchResults(items);
+	items = filterPlaylistItemsByTags(items);
+	items = sortPlaylistItems(items);
 
-	function desc(a: PlaylistItem, b: PlaylistItem) {
-		const { timeA, timeB } = getPublishTime(a, b);
-
-		return timeB - timeA;
-	}
-
-	function asc(a: PlaylistItem, b: PlaylistItem) {
-		const { timeA, timeB } = getPublishTime(a, b);
-
-		return timeA - timeB;
-	}
-});
-
-const playlistItems_Sorted_FilteredByTags = computed(() => {
-	if (!activeTagFilters.value.size) return playlistItems_Sorted.value;
-
-	return playlistItems_Sorted.value.filter(({ snippet }) => {
-		for (const tag of getVideoTags(snippet.resourceId.videoId)) {
-			if (activeTagFilters.value.has(tag)) return true;
-		}
-
-		return false;
-	});
+	return items;
 });
 
 const wholePlaylistFetched = computed(() => {
@@ -74,10 +46,10 @@ const wholePlaylistFetched = computed(() => {
 	);
 });
 
-// ==========
-
-const playlistItems_Sorted_FilteredByTagsAndSearchResults = computed(() => {
-	if (!searchInput.value) return playlistItems_Sorted_FilteredByTags.value;
+function filterPlaystListItemsBySearchResults(
+	items: PlaylistItems_ListResponse["items"],
+) {
+	if (!searchInput.value) return items;
 
 	const splitString_ToLowerCase = (str: string) => {
 		return [...str.split(" "), ...str.split(".")].map((word) =>
@@ -85,8 +57,8 @@ const playlistItems_Sorted_FilteredByTagsAndSearchResults = computed(() => {
 		);
 	};
 
-	return playlistItems_Sorted_FilteredByTags.value.filter((item) => {
-		let { title, videoOwnerChannelTitle: channel } = item.snippet;
+	return items.filter((item) => {
+		let { title, videoOwnerChannelTitle: channel, description } = item.snippet;
 
 		if (!item.contentDetails.videoPublishedAt) return false;
 
@@ -96,13 +68,34 @@ const playlistItems_Sorted_FilteredByTagsAndSearchResults = computed(() => {
 			switch (true) {
 				case title.toLowerCase().includes(lowercaseSearchWord):
 				case channel.toLowerCase().includes(lowercaseSearchWord):
+				case description.toLowerCase().includes(lowercaseSearchWord):
 					return true;
 			}
 		}
 
 		return false;
 	});
-});
+}
+
+function filterPlaylistItemsByTags(items: PlaylistItems_ListResponse["items"]) {
+	if (!activeTagFilters.value.size) return items;
+
+	return items.filter(({ snippet }) => {
+		for (const tag of getVideoTags(snippet.resourceId.videoId)) {
+			if (activeTagFilters.value.has(tag)) return true;
+		}
+
+		return false;
+	});
+}
+
+function sortPlaylistItems(items: PlaylistItems_ListResponse["items"]) {
+	return items.toSorted((a, b) => {
+		return sortDescending.value
+			? a.snippet.position - b.snippet.position
+			: b.snippet.position - a.snippet.position;
+	});
+}
 
 // ==========
 
@@ -122,44 +115,33 @@ onMounted(() => {
 				return;
 			}
 
-			fetchPlaylist(activeYear.value);
+			fetchCurrentPlaylist();
 		},
 		{ root: null, threshold: 0 },
 	).observe(bottom.value);
 });
+
+function fetchCurrentPlaylist() {
+	if (wholePlaylistFetched.value) return;
+
+	fetchPlaylist(activeYear.value);
+}
 </script>
 
 <template>
 	<div class="grid-content">
 		<div
-			class="grid-content-outter relative grid grid-cols-1 grid-rows-[2rem,_auto_2rem] gap-3"
+			class="grid-content-outter relative grid grid-cols-1 grid-rows-[2rem_auto_2rem] gap-3"
 		>
-			<div
-				class="relative col-start-1 col-end-2 row-start-1 grid place-items-center"
-			>
-				<div class="absolute bottom-0 left-0">
-					{{ playlistItems_Sorted_FilteredByTagsAndSearchResults.length }}/{{
-						currentPlaylist.data?.pageInfo?.totalResults
-					}}
-				</div>
-
-				<button
-					:disabled="currentPlaylist.fetching"
-					:class="[
-						{ hidden: sortDescending || wholePlaylistFetched },
-						'self-stretch rounded bg-white px-3 shadow disabled:opacity-50',
-					]"
-					@click="fetchPlaylist(activeYear)"
-				>
-					load more videos
-				</button>
-			</div>
+			<span class="z-10 col-[1/2] row-start-1 self-end justify-self-start">
+				{{ currentPlaylist.data?.items?.length ?? " - " }}/{{
+					currentPlaylist.data?.pageInfo?.totalResults ?? " - "
+				}}
+			</span>
 
 			<div class="row-start-2 grid gap-5 md:grid-cols-2 lg:grid-cols-3">
 				<PlaylistItem_Component
-					v-for="(
-						item, _
-					) in playlistItems_Sorted_FilteredByTagsAndSearchResults"
+					v-for="(item, _) in currentPlaylistItems"
 					:key="item.contentDetails.videoId"
 					v-bind="item"
 					@click="setActiveVideo(item)"
@@ -168,15 +150,22 @@ onMounted(() => {
 
 			<div ref="bottom" />
 
-			<Transition name="fade">
-				<div
-					v-show="currentPlaylist.fetching"
-					:class="[
-						sortDescending ? 'row-start-3' : 'row-start-1',
-						'loading pointer-events-none -z-10 col-start-1 rounded bg-stone-300',
-					]"
-				/>
-			</Transition>
+			<div
+				v-if="!wholePlaylistFetched"
+				:class="[
+					{ loading: currentPlaylist.fetching },
+					sortDescending ? 'row-start-3' : 'row-start-1',
+					'place-items-center/ col-[1/2] grid w-full place-items-center rounded',
+				]"
+			>
+				<button
+					:disabled="currentPlaylist.fetching"
+					class="rounded bg-white px-3 font-semibold text-neutral-700 shadow disabled:hidden"
+					@click="fetchCurrentPlaylist"
+				>
+					fetch videos
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
