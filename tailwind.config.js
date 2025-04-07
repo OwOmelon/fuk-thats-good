4200eccc20b8a138196c0d2f381287af72c71b8c
melon <cccaaarrrlll99@gmail.com>
2025-04-07 13:39:36 +0800

    add custom tailwind class for debugging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from "tailwindcss/plugin";
+
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities }) {
+      matchUtilities({
+        "debug-outline": (color) => {
+          return {
+            outlineStyle: "solid",
+            outlineWidth: "4px",
+            outlineColor: color,
+          };
+        },
+      });
+    }),
+  ],
 };
