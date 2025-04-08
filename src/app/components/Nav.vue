0b44a580d26442d4ae89a40c7bcf3cfd92bc73b0
melon <cccaaarrrlll99@gmail.com>
2025-04-08 13:44:15 +0800

    link nav github icon to repo

--- src/app/components/Nav.vue
+++ src/app/components/Nav.vue
@@ -3,15 +3,18 @@ import IconGithub from "~icons/mdi/github";
 </script>
 
 <template>
-	<div class="w-screen !max-w-[initial] mb-5 bg-stone-50 drop-shadow-[0_0.25em_0.25em_rgba(0,_0,_0,_12.5%)]">
-		<div
-			class="max-w-screen-xl grid-content mx-auto"
-		>
+	<div
+		class="mb-5 w-screen !max-w-[initial] bg-stone-50 drop-shadow-[0_0.25em_0.25em_rgba(0,_0,_0,_12.5%)]"
+	>
+		<div class="grid-content mx-auto max-w-screen-xl">
 			<div
 				class="grid-content-outter flex justify-between py-6 text-xl min-[375px]:text-2xl"
 			>
 				<span class="font-bold">FUK THAT'S GOOD</span>
-				<IconGithub />
+
+				<a href="https://github.com/OwOmelon/fuk-thats-good" target="_blank">
+					<IconGithub />
+				</a>
 			</div>
 		</div>
 	</div>
