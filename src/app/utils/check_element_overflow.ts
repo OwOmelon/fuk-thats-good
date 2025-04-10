e4bfdc10cf10cf47881904367a88561bbafb7521
melon <cccaaarrrlll99@gmail.com>
2025-04-10 10:36:29 +0800

    create new app util "checkElementOverflow"

---
+++ src/app/utils/check_element_overflow.ts
@@ -0,0 +1,12 @@
+export function checkElementOverflow(el: HTMLElement) {
+	const curOverflow = el.style.overflow;
+
+	if (!curOverflow || curOverflow === "visible") el.style.overflow = "hidden";
+
+	const isOverflowing =
+		el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight;
+
+	el.style.overflow = curOverflow;
+
+	return isOverflowing;
+}
