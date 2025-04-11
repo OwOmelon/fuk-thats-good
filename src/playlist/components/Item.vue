78a9c4d01de375304ee1209affefb8809d39f213
melon <cccaaarrrlll99@gmail.com>
2025-04-11 17:24:38 +0800

    revamp the look of playlist items

--- src/playlist/components/Item.vue
+++ src/playlist/components/Item.vue
@@ -2,18 +2,53 @@
 import IconPlay from "~icons/material-symbols/play-arrow-rounded";
 import TagIcon from "../components/TagIcon.vue";
 
-import { useTemplateRef, computed } from "vue";
-import { formatDistance } from "date-fns";
-
+import { useTemplateRef, ref, onMounted } from "vue";
+import { format, formatDistance } from "date-fns";
 import { addNewError } from "@/errors/store";
 import { getVideoTags } from "../utils/get_video_tags";
+import { useToolTip } from "@/app/composables/use_tooltip";
+import { checkElementOverflow } from "@/app/utils/check_element_overflow";
 
 import type { PlaylistItem } from "../types";
+
+// ==========
 
 const { snippet, contentDetails } = defineProps<{
 	snippet: PlaylistItem["snippet"];
 	contentDetails: PlaylistItem["contentDetails"];
 }>();
+
+const itemRef = useTemplateRef("itemRef");
+
+const tagIds = getVideoTags(snippet.resourceId.videoId);
+const videoUrl = `https://www.youtube.com/watch?v=${snippet.resourceId.videoId}`;
+
+const publishedAt_Formatted = (() => {
+	const date = new Date(contentDetails.videoPublishedAt);
+
+	return {
+		date: format(date, "d MMM yyyy"),
+		time: format(date, "HH:mm:ss OOOO"),
+		distance: `${formatDistance(new Date(), new Date(date))} ago`,
+	};
+})();
+
+onMounted(() => {
+	if (!itemRef.value) return;
+
+	const { thumbnails } = snippet;
+	const url =
+		thumbnails?.maxres?.url ||
+		thumbnails?.standard?.url ||
+		thumbnails?.high?.url ||
+		thumbnails?.medium?.url ||
+		thumbnails?.default?.url ||
+		"";
+
+	itemRef.value.style.setProperty("--thumbnail-url", `url(${url})`);
+});
+
+// ==========
 
 const videoUrlCopiedToast = {
 	ref: useTemplateRef<HTMLElement>("vuct"),
@@ -47,35 +82,10 @@ const videoUrlCopiedToast = {
 	},
 };
 
-const tagIds = computed(() => {
-	return getVideoTags(snippet.resourceId.videoId);
-});
-
-const videoUrl = computed(() => {
-	return `https://www.youtube.com/watch?v=${snippet.resourceId.videoId}`;
-});
-
-const thumbnailUrl = computed(() => {
-	const { thumbnails } = snippet;
-
-	return (
-		thumbnails?.maxres?.url ||
-		thumbnails?.standard?.url ||
-		thumbnails?.high?.url ||
-		thumbnails?.medium?.url ||
-		thumbnails?.default?.url ||
-		""
-	);
-});
-
-function formatDate(date: Date) {
-	return date ? `${formatDistance(new Date(), new Date(date))} ago` : "---";
-}
-
 async function writeVideoUrlToClipboard() {
 	try {
 		await navigator.clipboard.write([
-			new ClipboardItem({ "text/plain": videoUrl.value }),
+			new ClipboardItem({ "text/plain": videoUrl }),
 		]);
 
 		videoUrlCopiedToast.anim();
@@ -89,10 +99,6 @@ async function writeVideoUrlToClipboard() {
 
 // ==========
 
-import { ref } from "vue";
-import { checkElementOverflow } from "@/app/utils/check_element_overflow";
-import { useToolTip } from "@/app/composables/use_tooltip";
-
 const titleTooltip = {
 	ref: useTemplateRef<HTMLElement>("title"),
 	text: ref(`${snippet.title}`),
@@ -105,9 +111,7 @@ const channelTooltip = {
 
 const publishedAtTooltip = {
 	ref: useTemplateRef<HTMLElement>("publishedAt"),
-	text: ref(
-		`${new Date(contentDetails.videoPublishedAt).toLocaleDateString()}`,
-	),
+	text: ref(`${publishedAt_Formatted.date} | ${publishedAt_Formatted.time}`),
 };
 
 useToolTip(titleTooltip.ref, titleTooltip.text, () =>
@@ -123,22 +127,19 @@ useToolTip(publishedAtTooltip.ref, publishedAtTooltip.text);
 
 <template>
 	<div
+		ref="itemRef"
 		class="flex rounded-md border-2 border-stone-200 bg-stone-200 shadow shadow-stone-400/50"
 	>
 		<a
 			:href="videoUrl"
 			target="_blank"
-			class="group rounded-[inherit] drop-shadow-[0.125rem_0_0.125rem_rgba(0,_0,_0,_0.125)]"
+			class="group z-10 rounded-[inherit] drop-shadow-[0.125rem_0_0.125rem_rgba(0,_0,_0,_0.125)]"
 		>
 			<div
-				class="relative grid h-20 w-20 place-items-center overflow-hidden rounded-[inherit] border-2 border-stone-100 bg-black"
+				class="relative grid h-24 w-24 place-items-center overflow-hidden rounded-[inherit] border-2 border-stone-100 bg-black"
 			>
 				<div
-					:style="{
-						backgroundImage: `url(${thumbnailUrl})`,
-						backgroundSize: '180%',
-					}"
-					class="h-full w-full bg-center bg-no-repeat shadow-[inset_0_0_0.25rem_rgba(0,_0,_0,_0.125)] transition-opacity group-hover:opacity-75"
+					class="video-thumbnail-bg h-full w-full shadow-[inset_0_0_0.25rem_rgba(0,_0,_0,_0.125)] transition-opacity group-hover:opacity-75"
 				/>
 
 				<IconPlay
@@ -147,52 +148,79 @@ useToolTip(publishedAtTooltip.ref, publishedAtTooltip.text);
 			</div>
 		</a>
 
-		<div class="grid grow grid-cols-[auto,_1rem] gap-2.5 p-2.5">
-			<div class="relative mr-5 flex grow flex-col items-start text-stone-700">
+		<div
+			class="item-info relative isolate flex grow flex-col items-start p-2.5 py-1 text-xs text-stone-700"
+		>
+			<span
+				ref="title"
+				class="mb-auto cursor-pointer text-sm font-semibold"
+				@click="writeVideoUrlToClipboard"
+			>
+				{{ snippet.position + 1 }}. {{ snippet.title }}
+			</span>
+
+			<a
+				ref="channel"
+				:href="`https://www.youtube.com/channel/${snippet.videoOwnerChannelId}`"
+				target="_blank"
+				class="secondary-text line-clamp-1"
+			>
+				{{ snippet.videoOwnerChannelTitle }}
+			</a>
+
+			<time
+				ref="publishedAt"
+				:dateTime="contentDetails.videoPublishedAt"
+				class="secondary-text"
+			>
+				{{ publishedAt_Formatted.distance }}
+			</time>
+
+			<div class="flex items-center gap-2">
 				<span
-					ref="title"
-					class="relative mb-auto cursor-pointer text-xs font-semibold"
-					@click="writeVideoUrlToClipboard"
+					class="rounded bg-stone-600 px-1 text-xs font-semibold text-stone-200"
+					>TAGS</span
 				>
-					{{ snippet.position + 1 }}. {{ snippet.title }}
-				</span>
 
-				<a
-					ref="channel"
-					:href="`https://www.youtube.com/channel/${snippet.videoOwnerChannelId}`"
-					target="_blank"
-					class="secondary-text line-clamp-1"
-				>
-					{{ snippet.videoOwnerChannelTitle }}
-				</a>
-
-				<span ref="publishedAt" class="secondary-text">
-					{{ formatDate(contentDetails.videoPublishedAt) }}
-				</span>
-
-				<div
-					ref="vuct"
-					class="pointer-events-none absolute bottom-full left-0 mb-2 bg-stone-800 px-2 py-1 text-xs font-semibold text-stone-200 opacity-0"
-				>
-					VIDEO URL COPIED
+				<div class="flex gap-1">
+					<TagIcon v-for="id in tagIds" :id="id" add-tooltip class="w-3" />
 				</div>
 			</div>
 
-			<div class="grid grid-rows-4">
-				<div v-for="id in tagIds" class="relative grid place-items-center">
-					<TagIcon :id="id" class="absolute h-4 w-4 rotate-90" />
-				</div>
+			<div
+				ref="vuct"
+				class="pointer-events-none absolute bottom-full left-0 mb-2 bg-stone-800 px-2 py-1 text-xs font-semibold text-stone-200 opacity-0"
+			>
+				VIDEO URL COPIED
 			</div>
 		</div>
 	</div>
 </template>
 
 <style scoped lang="postcss">
-span {
-	@apply line-clamp-1;
+.video-thumbnail-bg {
+	background-image: var(--thumbnail-url);
+	background-size: 180%;
+
+	@apply bg-center bg-no-repeat;
 }
 
-.secondary-text {
-	@apply text-xs opacity-80;
+.item-info::before {
+	content: "";
+	background-image: var(--thumbnail-url);
+	background-size: 180%;
+
+	@apply pointer-events-none absolute left-0 top-0 -z-20 h-full w-full rounded bg-center bg-no-repeat opacity-15;
+}
+
+.item-info::after {
+	content: "";
+	background-image: linear-gradient(to right, theme(colors.stone.200), #0000);
+
+	@apply pointer-events-none absolute left-0 top-0 -z-10 h-full w-full;
+}
+
+span {
+	@apply line-clamp-1;
 }
 </style>
